8b96678cb98f749923506f3a067c1f92a81de1ac
Michael Bombini <bombini.michael@gmail.com>
2022-06-24 13:49:39 +0200

    Created TheHeader component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,29 @@
+<template>
+<header class="p-3">
+    <nav class="">
+        <img src="../../public/logo-small.svg" alt="" class="logo">
+    </nav>
+</header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+header {
+    background-color: #2E3A46;
+
+
+.logo {
+    width: 60px;
+    height: 60px;
+}
+
+}
+
+
+</style>
