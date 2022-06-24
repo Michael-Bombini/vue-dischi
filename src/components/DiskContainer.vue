79776c7901cf751c2b6a4123f4793638e4b4200a
Michael Bombini <bombini.michael@gmail.com>
2022-06-24 15:29:03 +0200

    Completed disk

--- src/components/DiskContainer.vue
+++ src/components/DiskContainer.vue
@@ -1,48 +1,46 @@
 <template>
     <main class="flex-grow-1">
         <div class="container">
-    
+            <div class="row row-cols-5 g-4">
+                <div class="col" v-for="disk in diskList" :key="disk.title">
+                    <TheDisk :info="disk"/>
+                </div>
+           </div>
         </div>
     </main>
 </template>
 
 <script>
 import axios from "axios";
+import TheDisk from "./TheDisk.vue";
 
 
     export default {
-        name : "DiskContainer",
-
-    data(){
-        return{
-            diskList : [],
-        }
+    name: "DiskContainer",
+    data() {
+        return {
+            diskList: [],
+        };
     },
-
-    methods : {
-        fetchDisk(){
+    methods: {
+        fetchDisk() {
             axios.get("https://flynn.boolean.careers/exercises/api/array/music")
-            .then((response)=>{
+                .then((response) => {
                 this.diskList = response.data.response;
-            })
+            });
         }
     },
-
-
     mounted() {
         this.fetchDisk();
-    }
-
-
-
-
-
-    }
+    },
+    components: { TheDisk }
+}
 </script>
 
 <style lang="scss" scoped>
 main {
     background-color: #1E2D3B;
     height: 90vh;
+    padding: 7rem 0;
 }
 </style>
--- src/components/TheDisk.vue
+++ src/components/TheDisk.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+            <img :src="info.poster" alt="">
+            <h1>{{info.title}}</h1>
+            <h2>{{info.author}}</h2>
+            <h3>{{info.year}}</h3>
 
     </div>
 </template>
@@ -7,9 +11,40 @@
 <script>
     export default {
         name : "TheDisk",
+        props : {
+            info : Object,
+        }
     }
 </script>
 
 <style lang="scss" scoped>
 
+div {
+    height: 350px;
+    background: #2E3A46;
+    padding: 0.5rem;
+    text-align: center;
+
+
+img {
+    aspect-ratio: 1/1;
+    height: 180px;
+    max-width: 100%;
+    margin-bottom: 0.9rem;
+}
+
+h1 {
+    font-size: 1.4rem;
+    text-align: center;
+    color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+}
+
+h2 , h3{
+    font-size: 1.2rem;
+    color: #6D6B6B;
+}
+
+}
 </style>
